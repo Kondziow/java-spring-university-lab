8df641b27d171808c3ed91d88c5e0b8105730b67
Kondziow <konradwojtanowski02@gmail.com>
2023-10-30 10:56:40 +0100

    lab3 initializr

---
+++ Lab3D/src/main/java/com/example/Lab3D/Lab3DApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lab3D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3DApplication.class, args);
+	}
+
+}
