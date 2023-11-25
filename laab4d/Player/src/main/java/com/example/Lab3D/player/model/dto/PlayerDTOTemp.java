13ca59d0291cd52f50055e8abd8e8b600ac91093
Kondziow <101991466+Kondziow@users.noreply.github.com>
2023-11-25 16:21:52 +0100

    Delete laab4d directory

--- laab4d/Player/src/main/java/com/example/Lab3D/player/model/dto/PlayerDTOTemp.java
+++
@@ -1,9 +0,0 @@
-package com.example.Lab3D.player.model.dto;
-
-import java.util.UUID;
-
-public class PlayerDTOTemp {
-    private UUID id;
-    private String name;
-    private Integer abilities;
-}
